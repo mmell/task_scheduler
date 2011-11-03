6bfc3d3367d931887946a1d7230e51bd3c676124
Mike Mell <mike@linksafe.name>
2011-11-03 14:47:37 -0700

    require rails 3.1.1

--- test/scheduled_tasks_test.rb
+++ test/scheduled_tasks_test.rb
@@ -1,15 +1,15 @@
-require File.dirname(__FILE__) + '/test_helper' 
+require 'test_helper'
 
 class ScheduledTasksTest < ActiveSupport::TestCase
 
   def setup
-    @knowns = { 
+    @knowns = {
       :rake_method => 'task_scheduler:run',
       :interval_seconds => 3600,
       :now => Time.now
     }
   end
-  
+
   def test_this_plugin
     ScheduledTask.create(:rake_method => @knowns[:rake_method], :interval_seconds => @knowns[:interval_seconds], :next_run => @knowns[:now] )
     scheduled_task = ScheduledTask.find(:first)
