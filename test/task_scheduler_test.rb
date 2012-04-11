dbfcffc350ab33e977553699100fec86b4a2028a
Michael Mell <mike.mell@nthwave.net>
2012-04-11 13:49:39 -0700

    clean up

--- test/task_scheduler_test.rb
+++ test/task_scheduler_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TaskSchedulerTest < ActiveSupport::TestCase
   def test_invoke
-    raise RuntimeError, 'need to test that bundle exec Rake::Task[e.rake_method].invoke works properly in rake task_scheduler:run'
+    flunk 'need to test that bundle exec Rake::Task[e.rake_method].invoke works properly in rake task_scheduler:run'
   end
 
 
